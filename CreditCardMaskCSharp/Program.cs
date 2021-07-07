fa480f2dfb18d9e7158eccdfed360e24d1e6dec3
David Parker <davidparker333@gmail.com>
2021-07-07 17:56:21 -0400

    Add project files.

---
+++ CreditCardMaskCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreditCardMaskCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct.
+            //However, since someone could look over your shoulder, you don't want that shown on your screen.Instead, we mask it.
+            //Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+            string cc = "4556364607935616";
+
+            Console.WriteLine(Maskify(cc));
+
+        }
+
+        public static string Maskify(string cc)
+        {
+            if(cc.Length < 4)
+            {
+                return cc;
+            }
+            string lastFour = cc.Substring(cc.Length-4);
+            List<string> hash = new List<string>();
+            for (int i=0; i<cc.Length-4; i++)
+            {
+                hash.Add("#");
+            }
+            string stringHash =  String.Join("", hash.ToArray());
+            return stringHash + lastFour;
+        }
+    }
+}
